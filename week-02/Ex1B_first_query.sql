021bbffdbd2d797425619808dc7c0cffbb4e2883
dmelgar333 <dmelgar@my.yearupunited.org>
2026-04-17 14:57:36 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -2,3 +2,6 @@
 Sakila is a database used to collect information about tables and topics for use by practicing.
 */
 
+USE sakila;
+SELECT * FROM actor; -- Retrieved [number] records
+SELECT * FROM film; -- Retrieved [number] records
