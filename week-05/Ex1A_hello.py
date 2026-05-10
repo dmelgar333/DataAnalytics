af2b13f7ef4ea48452d79e9d36ded67aa2a84c84
dmelgar333 <dmelgar@my.yearupunited.org>
2026-05-10 15:28:48 -0400

    updated file

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,18 @@
 print('Hello world!')
+
+message = 'Hello world'
+print(message)
+#printing twice because both are statements different versions same output. 
+
+#Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+#What I noticed about the results is its very straightforward, you ask for what you want the results to be. 
+
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + "" + c_str)
